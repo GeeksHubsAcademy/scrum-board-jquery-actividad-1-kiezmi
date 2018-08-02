563666d9b7cad028e156c5b83b0f86f2cb92c444
Enrique de Miguel <Kiezmi@gmail.com>
2018-08-02 17:21:21 +0200

    tareas simples

--- app.js
+++ app.js
@@ -1,77 +1,49 @@
 $(document).ready(function () {
     let addListInput = $('.addListWrapper input');
-    const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`
+    let addListButton = $('.addListWrapper button');
+    const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`;
     const createListString = name =>
         `<div class="list" id="${generateId('list')}">
             <div class="listHeader">
                 <h4>${name}</h4>
-                <button type="submit">X</button>
+                <button type="button" class="close">X</button>
             </div>
             <div class="tasks">
                
             </div>
             <div class="addTask">
                 <input type="text">
-                <button>Add task</button>
+                <button type="button" class="suma">Add task</button>
             </div>
         </div>`
 
     const appendNewList = () => {
-        //  cogemos el text del input
-        let listName = addListInput.val();
+        //  cogemos el text del input si no esta vacio y le quitamos 
+        //los espacios por la parte de la derecha.
+        if (addListInput.val() === '') {
+            return;
+        }
+        let listName = addListInput.val().trim();
 
         // creamos el nodo .list
         let list = $(createListString(listName));
 
         // añadimos el node al DOM
-        $('.lists').append(list)
+        $('.lists').append(list);
 
         // Limpiamos el texto del input
         addListInput.val('');
     }
-
-    /* const appendNewTasks = () => {
-        //  cogemos el text del input
-        let tasksName = addTasksInput.val();
-
-        // creamos el nodo .task
-        let tasks = $(createTasksString(tasksName));
-
-        // añadimos el node al DOM
-        $('.task').append(tasks)
-
-        // Limpiamos el texto del input
-        addTasksInput.val('');
-    } */
-
-
-    // Listeners
+        // Listeners
     addListInput.on('keyup', function (event) {
         if (event.keyCode === 13) {
             appendNewList();
         }
     })
-
-    /*  addListButton.on('click', function (event) {
-         // if (event.keyCode === 13) {
-         appendNewList();
-         // }
-     }) */
-
-    $('.lists').on('click', '.listHeader button', function (event) {
+   
+    $('.lists').on('click', '.listHeader', function (event) {
         let listNode = $(event.target.parentNode.parentNode);
         listNode.detach();
     })
-    addTasksInput.on('keyup', function (event) {
-        if (event.keyCode === 13) {
-            appendNewTasks();
-        }
-
-    //     $('.task').on('click', '.task button', function (event) {
-    //         let listNode = $(event.target.parentNode.parentNode);
-    //         listNode.detach();
-     })
-
-
 
 })
