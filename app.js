9a452e19d6a86b526d33aa2c288b116d5d1cfd3c
Enrique de Miguel <Kiezmi@gmail.com>
2018-08-06 16:42:42 +0200

    añade tarea pero no borra

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-/////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////  Listas  //////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-$(document).ready(function () {
-    let addListInput = $('.addListWrapper input');
-    let addListButton = $('.addListWrapper button');
-    const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`;
-    const createListString = name =>
-        `<div class="list" id="${generateId('list')}">
+  ///////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////  Listas  //////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////
+  $(document).ready(function () {
+      let addListInput = $('.addListWrapper input');
+      let addListButton = $('.addListWrapper button');
+      const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`;
+      const createListString = name =>
+          `<div class="list" id="${generateId('list')}">
             <div class="listHeader">
                 <h4>${name}</h4>
                 <button type="button">X</button>
@@ -22,85 +22,88 @@ $(document).ready(function () {
             </div>
         </div>`
 
-    const appendNewList = () => {
-        //  cogemos el text del input si no esta vacio y le quitamos 
-        //los espacios por la parte de la derecha.
-        if (addListInput.val().trim() === '') {
-            return addListInput.val('');
-        }
-        let listName = addListInput.val();
+      const appendNewList = () => {
+          //  cogemos el text del input si no esta vacio y le quitamos 
+          //los espacios por la parte de la derecha.
+          if (addListInput.val().trim() === '') {
+              return addListInput.val('');
+          }
+          let listName = addListInput.val();
 
-        // creamos el nodo .list
-        let list = $(createListString(listName));
+          // creamos el nodo .list
+          let list = $(createListString(listName));
 
-        // añadimos el node al DOM
-        $('.lists').append(list);
+          // añadimos el node al DOM
+          $('.lists').append(list);
 
-        // Limpiamos el texto del input
-        addListInput.val('');
-    }
-    // Listeners
-    addListInput.on('keyup', function (event) {
-        if (event.keyCode === 13) {
-            appendNewList();
-        }
-    });
+          // Limpiamos el texto del input
+          addListInput.val('');
+      }
+      // Listeners
+      addListInput.on('keyup', function (event) {
+          if (event.keyCode === 13) {
+              appendNewList();
+          }
+      });
 
-    addListButton.on('click', function () {
-        appendNewList();
-    });
+      addListButton.on('click', function () {
+          appendNewList();
+      });
 
-    $('.lists').on('click', '.listHeader', function (event) {
-        let listNode = $(event.target.parentNode.parentNode);
-        listNode.detach();
-    })
-    ///////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////////////   Tareas   ///////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////////////
+      $('.lists').on('click', '.listHeader', function (event) {
+          let listNode = $(event.target.parentNode.parentNode);
+          listNode.detach();
+      })
+      /////////////////////////////////////////////////////////////////////////////////
+      //////////////////////////////   Tareas   ///////////////////////////////////////
+      /////////////////////////////////////////////////////////////////////////////////
 
-    let addTaskInput = $('.addTask input');
-    let addTaskButton = $('.addTask button');
-
-    const createTaskString = name =>
-        `<div class="task">
-            <div class="taskbody">
+      const createTaskString = name =>
+          `<div class="task">
+            <div class="showTask">
                 <h6>${name}</h6>
                 <button type="button">X</button>
             </div>
         </div>`
 
-    const appendNewTask = (e) => {
-        //  cogemos el text del input si no esta vacio y le quitamos 
-        //los espacios por la parte de la derecha.
-        if (addTaskInput.val().trim() === '') {
-            return;
-        }
-        let taskName = addTaskInput.val();
+      const appendNewTask = (e) => {
+          console.log("inicio", e.target.value);
+          //  cogemos el text del input si no esta vacio y le quitamos 
+          //los espacios por la parte de la derecha.
+          let addTaskInput = e.target;
+          if (addTaskInput.value.trim() === '') {
+              return;
+          }
+          console.log(addTaskInput);
+          let taskName = addTaskInput.value;
 
-        // creamos el nodo .task
-        let task = $(createTaskString(taskName));
+          // creamos el nodo .task
+          let task = $(createTaskString(taskName));
+          console.log(task);
+          // añadimos el node al DOM
+          let parent = $(e.target.parentNode.parentNode).find('.tasks').append(task);
+          console.log(parent);
+          parent;
+          // Limpiamos el texto del input
+          addTaskInput.value = '';
+      }
 
-        // añadimos el node al DOM
-        e.target.parentNode.parentNode.queryselector('.tasks').append('task');
-        // Limpiamos el texto del input
-        addTaskInput.val('');
-    }
-    // Listeners
+      // Listeners//
 
-    $(document).on('keyup', '.addTask input', function (event) {
-        if (event.keyCode === 13) {
-            appendNewTask(event);
-        }
-    });
+      $(document).on('keyup', '.addTask input', function (event) {
+          if (event.keyCode === 13) {
+              appendNewTask(event);
+          }
+      });
 
-    $(document).on('click', '.addTask button ', function (e) {
-        appendNewTask(e);
-    });
+      $(document).on('click', '.addTask button ', function (event) {
+          appendNewTask(event);
+      });
 
-    $(document).on('click', '.taskbody button', function (event) {
-        let taskNode = $(event.target.parentNode.parentNode);
+      $(document).on('click', '.showTask button', function (event) {
+          let taskNode = $(event.target.parentNode.parentNode);
 
-        taskNode.detach();
-    })
+          taskNode.detach();
+      })
 
-})
+  })
